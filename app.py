a2a5860f3c000232d90ad6afaae2e9bb296fcc3a
kingofholland <61987223+kingofholland@users.noreply.github.com>
2023-11-19 14:48:52 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import functions
 required_version = version.parse("1.1.1")
 current_version = version.parse(openai.__version__)
 load_dotenv()
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 if current_version < required_version:
     raise ValueError(f"Error: OpenAI version {openai.__version__} is less than the required version 1.1.1")
